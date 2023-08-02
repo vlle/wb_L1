e70b696ea0e689b2ecff4fa4404d2cb4c56c3f7d
artemiikulikov <aapqwhat@gmail.com>
2023-08-02 14:46:35 +0300

    :star: update: add 11-13

---
+++ 13.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+// Поменять местами два числа без создания временной переменной
+func t13(first, second int) {
+  fmt.Println("Before:", first, second)
+  // XOR: исключающее или
+  // example:
+  // initial state in bits:
+  // first: 101 
+  // second: 011
+  first ^= second
+  // first: 110
+  // second: 011
+  second ^= first
+  // first: 110
+  // second: 101
+  first ^= second
+  // first: 110
+  // second: 011
+  fmt.Println("After:", first, second)
+}
